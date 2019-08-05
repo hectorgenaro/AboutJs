9f3d594d6a05b1dc46b8340d7c5ce1bcf1568e78
hectorgenaro <hectorgenaro@gmail.com>
2019-08-04 23:58:43 -0500

    se agrega ejemplos de el libro eloquent de javascript

---
+++ eloquent.js
@@ -0,0 +1,21 @@
+
+//   let theNumber = Number(prompt("Pick a number"));
+//   if (!Number.isNaN(theNumber)) {
+//   alert("Your number is the square root of " +
+//   theNumber * theNumber);
+//   }
+// else {
+//   alert('Sorry it was not a number, reloaded the page a try again')
+// }
+
+// let number = 0;
+// while (number <= 12) {
+// console.log(number);
+// number = number + 2;
+// }
+let result = 1;
+let counter = 1;
+while (counter <= 10) {
+result = result * 2;
+counter = counter + 1;
+}
