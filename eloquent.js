dacb7bd0c980435535b953bbf718e03f86f6a96c
hectorgenaro <hectorgenaro@gmail.com>
2019-08-09 01:03:35 -0500

    se agrega estilos a pagina index para que no se vea tan solita

--- eloquent.js
+++ eloquent.js
@@ -1,4 +1,4 @@
-
+//
 //   let theNumber = Number(prompt("Pick a number"));
 //   if (!Number.isNaN(theNumber)) {
 //   alert("Your number is the square root of " +
@@ -7,15 +7,32 @@
 // else {
 //   alert('Sorry it was not a number, reloaded the page a try again')
 // }
-
+//
 // let number = 0;
 // while (number <= 12) {
 // console.log(number);
 // number = number + 2;
 // }
-let result = 1;
-let counter = 1;
-while (counter <= 10) {
-result = result * 2;
-counter = counter + 1;
+// let result = 1;
+// let counter = 1;
+// while (counter <= 10) {
+// result = result * 2;
+// counter = counter + 1;
+// }
+//
+// let yourName;
+// do {
+// yourName = prompt("Who are you?");
+// } while (!yourName);
+// alert('thanks' + ' ' + yourName);
+//
+  //programa que calculas 2 elevado a la decima potencia
+function exp(){
+  let result = 1;
+  for (let contador =0 ; contador < 10 ; contador ++) {
+    result = result * 2;
+  }
+  return result;
 }
+
+console.log(exp())
